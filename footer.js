8321bc5084a3a8c9ff7389d5e78d07662a022928
javierdosreis <94925907+javierdosreis@users.noreply.github.com>
2021-11-24 17:24:51 -0300

    Create footer.js

---
+++ footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './footer.css'
+
+function footer(_props){
+    return(
+        <footer>
+            <div className = "footer">
+                <p> Empresa </p>
+                <p> Caracteristicas </p>
+                <p> Recursos </p>
+                <p> Legal </p>
+            </div>
+            <div>
+                <p> Javier Dos Reis - Bootcamp 2021</p>
+            </div>
+        </footer>
+    )
+}
